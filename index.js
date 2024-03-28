4e6e75f5a2a998b42f514ec2cdfc4986057650ac
Erick Koome <koomembwiria@gmail.com>
2024-03-28 22:00:26 +0300

    control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someNumber){
   // Write your code here!
+  if (someNumber <= 400){
+    return "This one is on me!"
+  }
+  else if (someNumber > 400 && someNumber < 2000){
+    return "That will be twenty bucks."
+  }
+  else if (someNumber > 2000 && someNumber < 2500){
+    return "I will gladly take your thirty bucks."
+  }
+  else if (someNumber > 2500){
+    return "No can do."
+  }
+}
+const city = "NYC"
+function ternaryCheckCity(city){
+  // Write your code here!
+  return city === "NYC" ? `Ok, sounds good.` : `No go.`;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+const tip = 'generous';
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+    return ('Thank you so much.');
+      
+      
+    case 'not as generous':
+    return ('Thank you.');
+      
+      
+    default:
+    return ('Bye.');
+  }
 }
